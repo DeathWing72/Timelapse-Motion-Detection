1484f384482321e0ab7fb66108a66e1930a62a8e
DeathWing72 <47579801+DeathWing72@users.noreply.github.com>
2019-02-20 11:10:21 -0800

    Add files via upload

--- src/FileOutput.java
+++ src/FileOutput.java
@@ -5,11 +5,21 @@ import java.io.IOException;
 public class FileOutput
 {
     private PrintWriter out;
-    public void setFile(File file)
+    public void setFiles(File file, String customLoc, String name, String ext)
     {
-        try {
-            out = new PrintWriter(new FileWriter(file.getAbsolutePath()+".csv"));
-        } catch (IOException e) {}
+    	if(customLoc.length() != 0)
+    	{
+    		try 
+    		{
+                out = new PrintWriter(new FileWriter(customLoc+"\\"+name+ext));
+            } catch (IOException e) {}
+    	}
+    	else
+    	{
+    		try {
+                out = new PrintWriter(new FileWriter(file.getAbsolutePath()+"\\"+name+ext));
+            } catch (IOException e) {}
+    	}
     }
     public void newPrint(String print)
     {
